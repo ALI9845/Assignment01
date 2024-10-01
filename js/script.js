68a17754c8528d2eefe14c9db3b12cae78fd1546
ALI9845 <Wadia3913@gmail.com>
2024-10-01 15:45:41 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
-//STEP1
+//STEP1//
 let somE_moNth
 function the_Month(){
 
 }
 let cuRrentMonth
 let suMmer_moNthlet 
-MyLibrary-function
+MyLibrary_function
+
+//STEP2//
+let car = "12.3"
+console.log(car)
+ // 
+ const name = 'ALI'
+ const greating = `Hello , ${name}`
+ console.log(greating)
+ //
+
+ // 
+ let v1 = true 
+ let v2 = new Boollean(true)
+ console.log('v1 = = v2 is' + (v1==v2) )
+ console.log('v1 = = = v2 is' + (v1===v2) )
+ //
+
+ //
+ const foot = null 
+ console.log(null)
+ 
+
 
 
